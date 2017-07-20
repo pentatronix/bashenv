513ccd757a730f42b7b2a64b1587eed790651c0b
Andre Lewis <andre.lewis@arrayent.com>
2017-07-20 11:18:32 -0700

    Added install commands to automatically pull down Vundle and install the plugins Also goes into YouComplete me and compiles for C/Javascript

--- install.sh
+++ install.sh
@@ -6,12 +6,16 @@ echo   .tmux
 echo   .vimrc
 read -n1 -r -p "Press any key to continue or ^C to cancel...." key
 sudo apt-get -y update && sudo apt-get upgrade
-sudo apt-get -y install cmus tmux powerline vim git elinks 
+sudo apt-get -y install cmus tmux powerline vim git elinks build-essential cmake python-dev python3-dev
 mkdir ~/bin
 mkdir ~/.tmux
 mkdir ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/nhdaly/tmux-better-mouse-mode ~/.tmux/
 cat bashrc >> ~/.bashrc
 cat tmux >> ~/.tmux.conf
 cat vimrc >> ~/.vimrc
-
+cat powerline >> ~/.config/powerline
+vim -c PluginInstall
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --tern-completer
